299f383214016bf3b36f4113b0cb1f250b5dcf7c
hzhwcmhf <hzhwcmhf@gmail.com>
2017-03-16 23:59:08 +0800

    fix

--- main.py
+++ main.py
@@ -54,6 +54,8 @@ def run(args):
 	
 	hidden, action = SlstmLayer(phrase, seq_length, input_dim = (args.dim_r, args.dim_rp), output_dim = (args.dim_h, args.dim_hp), policy = policy, dropout_keepprob = args.keep_drop, pooling = False, update = "straight")
 	
+	hidden = tflearn.lstm(embedding, args.dim_h, dynamic=True)
+	
 	predict_y = ClassifyLayer(hidden, dim = args.dim_c, keepdrop = args.keep_drop, activation='prelu')
 	
 	net = tflearn.regression(predict_y, optimizer='adagrad', learning_rate=args.learning_rate,
